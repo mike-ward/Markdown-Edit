01efef09be58eb7714de1999142e472373ac7153
mike-ward <mike@mike-ward.net>
2014-10-11 13:31:44 -0400

    Two panels

---
+++ src/MarkdownEdit/Preview.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace MarkdownEdit
+{
+    public partial class Preview
+    {
+        public Preview()
+        {
+            InitializeComponent();
+            SizeChanged += OnSizeChanged;
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs)
+        {
+            Browser.Width = ActualWidth - 2;
+            Browser.Height = ActualHeight - 2;
+        }
+    }
+}
