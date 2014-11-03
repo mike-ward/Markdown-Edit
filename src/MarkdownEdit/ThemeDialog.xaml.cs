04d1f8a568fc0620c3754f990659fa5575390df1
mike-ward <mike@mike-ward.net>
2014-11-02 21:18:55 -0500

    add missing file

---
+++ src/MarkdownEdit/ThemeDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows.Controls;
+using Newtonsoft.Json;
+
+namespace MarkdownEdit
+{
+    public partial class ThemeDialog
+    {
+        public ThemeDialog()
+        {
+            InitializeComponent();
+            var assemblyFolder = Utility.AssemblyFolder();
+            var path = Path.Combine(assemblyFolder, "Themes");
+            var files = Directory.EnumerateFiles(path, "*.json");
+            ThemeListBox.ItemsSource = files
+                .Select(f => JsonConvert.DeserializeObject<Theme>(File.ReadAllText(f)))
+                .Select(t => new ListBoxItem {Tag = t, Content = t.Name});
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MainWindow.LoadThemeCommand.Execute(((ListBoxItem)ThemeListBox.SelectedItem).Tag, Owner);
+        }
+    }
+}
