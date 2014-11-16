0cde77054b158b9973cdfed8f22f0b52162fac20
mike-ward <mike@mike-ward.net>
2014-11-16 11:15:21 -0500

    vs not adding files again

---
+++ src/MarkdownEdit/Controls/ImportFromDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace MarkdownEdit
+{
+
+    public partial class ImportFromDialog
+    {
+        public static RoutedCommand ShowProviderFilePickerCommand = new RoutedCommand();
+
+        public ImportFromDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteShowProviderFilePicker(object sender, ExecutedRoutedEventArgs e)
+        {
+            var remoteProvider = (IRemoteProvider)e.Parameter;
+            remoteProvider.OpenFilePickerAsync();
+        }
+    }
+}
