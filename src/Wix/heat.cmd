74a28033bf2c8dd80c9758bf313b61022cea1ab0
mike-ward <mike@mike-ward.net>
2014-12-07 16:40:31 -0500

    Wix installer

---
+++ src/Wix/heat.cmd
@@ -0,0 +1,1 @@
+"c:\Program Files (x86)\WiX Toolset v3.9\bin\heat" dir "..\MarkdownEdit\bin\Release" -cg "MardownEditFiles" -gg -scom -suid -sreg -sfrag -srd -dr INSTALLFOLDER -var var.SourceFiles -out Files.wxs 
