67a1b8e78b0a37f9a1298998e79f583809c2ee99
mike-ward <mike@mike-ward.net>
2014-12-07 18:03:50 -0500

    Add fonts to installer

--- build.cmd
+++ build.cmd
@@ -1,5 +1,8 @@
 cd src
 msbuild MarkdownEdit\markdownedit.csproj /tv:14.0 /t:Rebuild /p:configuration=release
-echo %errorlevel%
-if NOT ERRORLEVEL 1 msbuild MarkdownEdit.sln /t:Setup /p:configuration=release
+if ERRORLEVEL 1 goto END
+cd Wix
+call build.cmd
+cd ..
+:END
 cd ..
