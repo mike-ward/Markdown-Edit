2b5c59f4e9cb6a44e21a854f3a1a2f19d39ccd6b
mike-ward <mike@mike-ward.net>
2014-12-08 09:41:16 -0500

    add ui to installer

--- src/Wix/build.cmd
+++ src/Wix/build.cmd
@@ -1,2 +1,2 @@
 @"c:\Program Files (x86)\WiX Toolset v3.9\bin\candle.exe" Product.wxs -dSourceFiles="..\MarkdownEdit\bin\Release" -out out\
-@if NOT ERRORLEVEL 1 "c:\Program Files (x86)\WiX Toolset v3.9\bin\light.exe" out\Product.wixobj  -out out\MarkdownEditSetup.msi
+@if NOT ERRORLEVEL 1 "c:\Program Files (x86)\WiX Toolset v3.9\bin\light.exe" -ext WixUIExtension out\Product.wixobj -out out\MarkdownEditSetup.msi
