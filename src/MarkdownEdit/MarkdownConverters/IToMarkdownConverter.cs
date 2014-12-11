0e519b52b5a146dc9362cfd3a672e2a0ebd3dfa8
mike-ward <mike@mike-ward.net>
2014-12-11 15:21:44 -0500

    Add interface to convert html to markdown

---
+++ src/MarkdownEdit/MarkdownConverters/IToMarkdownConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MarkdownEdit
+{
+    public interface IToMarkdownConverter
+    {
+        string ConvertToMarkdown(string from, string pathForRelativeUrls);
+    }
+}
