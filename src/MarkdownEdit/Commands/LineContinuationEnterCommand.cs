1e668e788a6355623366a2bcb63ec7e113fcec71
mike-ward <mike@mike-ward.net>
2015-01-03 18:17:58 -0500

    Can't leave it alone

--- src/MarkdownEdit/Commands/LineContinuationEnterCommand.cs
+++ src/MarkdownEdit/Commands/LineContinuationEnterCommand.cs
@@ -10,12 +10,12 @@ namespace MarkdownEdit
     {
         private readonly TextEditor _editor;
         private readonly ICommand _baseCommand;
-        private readonly Regex _orderedListPattern = new Regex("^[ ]{0,3}(\\d+)([\\.\\)])(?=[ ]{1,3}\\S)", RegexOptions.Compiled);
-        private readonly Regex _orderedListEndPattern = new Regex("^[ ]{0,3}(\\d+)([\\.\\)])(?=[ ]{1,3}\\s*)", RegexOptions.Compiled);
-        private readonly Regex _unorderedListPattern = new Regex("^[ ]{0,3}[-\\*\\+](?=[ ]{1,3}\\S)", RegexOptions.Compiled);
-        private readonly Regex _unorderedListEndPattern = new Regex("^[ ]{0,3}[-\\*\\+](?=[ ]{1,3}\\s*)", RegexOptions.Compiled);
-        private readonly Regex _blockQuotePattern = new Regex("^(([ ]{0,4}>)+)[ ]{0,4}.{2}", RegexOptions.Compiled);
-        private readonly Regex _blockQuoteEndPattern = new Regex("^([ ]{0,4}>)+[ ]{0,4}\\s*", RegexOptions.Compiled);
+        private readonly Regex _orderedListPattern = new Regex(@"^[ ]{0,3}(\d+)\.(?=[ ]{1,3}\S)", RegexOptions.Compiled);
+        private readonly Regex _orderedListEndPattern = new Regex(@"^[ ]{0,3}(\d+)\.(?=[ ]{1,3}\s*)", RegexOptions.Compiled);
+        private readonly Regex _unorderedListPattern = new Regex(@"^[ ]{0,3}[-\*\+](?=[ ]{1,3}\S)", RegexOptions.Compiled);
+        private readonly Regex _unorderedListEndPattern = new Regex(@"^[ ]{0,3}[-\*\+](?=[ ]{1,3}\s*)", RegexOptions.Compiled);
+        private readonly Regex _blockQuotePattern = new Regex(@"^(([ ]{0,4}>)+)[ ]{0,4}.{2}", RegexOptions.Compiled);
+        private readonly Regex _blockQuoteEndPattern = new Regex(@"^([ ]{0,4}>)+[ ]{0,4}\s*", RegexOptions.Compiled);
 
         public LineContinuationEnterCommand(TextEditor editor, ICommand baseCommand)
         {
@@ -38,9 +38,10 @@ namespace MarkdownEdit
         {
             _baseCommand.Execute(parameter);
 
-            var line = _editor.Document.GetLineByOffset(_editor.SelectionStart)?.PreviousLine;
+            var document = _editor.Document;
+            var line = document.GetLineByOffset(_editor.SelectionStart)?.PreviousLine;
             if (line == null) return;
-            var text = _editor.Document.GetText(line.Offset, line.Length);
+            var text = document.GetText(line.Offset, line.Length);
 
             Func<Regex, Action<Match>, Action> matcher = (pattern, action) =>
             {
@@ -50,19 +51,12 @@ namespace MarkdownEdit
 
             var patterns = new[]
             {
-                matcher(_unorderedListPattern, m => _editor.Document.Insert(_editor.SelectionStart, m.Groups[0].Value.Trim() + " ")),
-                matcher(_unorderedListEndPattern, m => _editor.Document.Remove(line)),
-                matcher(_orderedListPattern, m =>
-                {
-                    int number;
-                    if (int.TryParse(m.Groups[1].Value, out number))
-                    {
-                        _editor.Document.Insert(_editor.SelectionStart, string.Format("{0}{1} ", ++number, m.Groups[2].Value.Trim()));
-                    }
-                }),
-                matcher(_orderedListEndPattern, m => _editor.Document.Remove(line)),
-                matcher(_blockQuotePattern, m => _editor.Document.Insert(_editor.SelectionStart, m.Groups[1].Value.TrimStart())),
-                matcher(_blockQuoteEndPattern, m => _editor.Document.Remove(line))
+                matcher(_unorderedListPattern, m => document.Insert(_editor.SelectionStart, m.Groups[0].Value.TrimStart() + " ")),
+                matcher(_unorderedListEndPattern, m => document.Remove(line)),
+                matcher(_orderedListPattern, m => document.Insert(_editor.SelectionStart, (int.Parse(m.Groups[1].Value) + 1) + ". ")),
+                matcher(_orderedListEndPattern, m => document.Remove(line)),
+                matcher(_blockQuotePattern, m => document.Insert(_editor.SelectionStart, m.Groups[1].Value.TrimStart())),
+                matcher(_blockQuoteEndPattern, m => document.Remove(line))
             };
 
             patterns.FirstOrDefault(action => action != null)?.Invoke();
