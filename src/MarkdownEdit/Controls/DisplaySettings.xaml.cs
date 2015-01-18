3efe376a239313bd3e9804c58d9e15eb90a86956
mike-ward <mike@mike-ward.net>
2015-01-18 17:04:23 -0500

    Add font section

---
+++ src/MarkdownEdit/Controls/DisplaySettings.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MarkdownEdit.Controls
+{
+    public partial class DisplaySettings : UserControl
+    {
+        public DisplaySettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
