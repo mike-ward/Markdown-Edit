1f9737ccf582bfd94c71eabaf0a2ff2029a471df
mike-ward <mike@mike-ward.net>
2015-03-31 19:43:40 -0400

    Release 1.4

--- src/MarkdownEdit-Package/Package/tools/ChocolateyInstall.ps1
+++ src/MarkdownEdit-Package/Package/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'markdown-edit' 'msi' '/qb SKIPFRAMEWORKCHECK="1"' 'https://github.com/mike-ward/Markdown-Edit/releases/download/v1.3.6/MarkdownEditSetup.msi'
+Install-ChocolateyPackage 'markdown-edit' 'msi' '/qb SKIPFRAMEWORKCHECK="1"' 'https://github.com/mike-ward/Markdown-Edit/releases/download/v1.4.0/MarkdownEditSetup.msi'
