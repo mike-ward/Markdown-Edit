e2af3db97818576049a697fffabb9677f64057a0
mike-ward <mike@mike-ward.net>
2015-05-28 20:17:46 -0400

    ordered/unorderd list command

--- src/MarkdownEdit/Models/EditorUtilities.cs
+++ src/MarkdownEdit/Models/EditorUtilities.cs
@@ -228,11 +228,12 @@ namespace MarkdownEdit.Models
         public static void ConvertSelectionToList(TextEditor textEditor)
         {
             var selection = textEditor.TextArea.Selection;
-            var start = selection.StartPosition.Line;
+            var start = Math.Min(selection.StartPosition.Line, selection.EndPosition.Line);
             if (start == 0) return;
-            var end = selection.EndPosition.Line;
+            var end = Math.Max(selection.StartPosition.Line, selection.EndPosition.Line);
             var document = textEditor.Document;
-            var startsWith = new Regex(@"\s*[-|\*|\+]\s{1,4}");
+            var ordered = new Regex(@"^\s*\d+\.\s{1,4}");
+            var unordered = new Regex(@"^\s*[-|\*|\+]\s{1,4}");
 
             textEditor.BeginChange();
             try
@@ -240,8 +241,23 @@ namespace MarkdownEdit.Models
                 foreach (var num in Enumerable.Range(start, end - start + 1))
                 {
                     var line = document.GetLineByNumber(num);
+                    var offset = line.Offset;
                     var text = document.GetText(line);
-                    if (string.IsNullOrWhiteSpace(text) == false && startsWith.IsMatch(text) == false)
+                    if (string.IsNullOrWhiteSpace(text)) continue;
+
+                    if (unordered.IsMatch(text))
+                    {
+                        var numbered = Regex.Replace(text, @"[-|\*|\+]", "1.");
+                        document.Remove(line);
+                        document.Insert(offset, numbered);
+                    }
+                    else if (ordered.IsMatch(text))
+                    {
+                        var unnumbered = Regex.Replace(text, @"\d+\.", "-");
+                        document.Remove(line);
+                        document.Insert(offset, unnumbered);
+                    }
+                    else
                     {
                         var indexOfFirstChar = text.TakeWhile(char.IsWhiteSpace).Count();
                         document.Insert(line.Offset + indexOfFirstChar, "- ");
