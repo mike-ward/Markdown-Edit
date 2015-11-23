26462acc4e23196dec3f975c52731737ad277532
mike-ward <mike@mike-ward.net>
2015-11-23 18:59:16 -0500

    fix build command to work on appveyor'

--- src/Wix/build.cmd
+++ src/Wix/build.cmd
@@ -1,5 +1,5 @@
-del /Q out\*.msi
+del /Q "out\*.msi"
 @"%WIX%\bin\candle.exe" Product.wxs -dSourceFiles="..\MarkdownEdit\bin\Release" -out out\
 @if NOT ERRORLEVEL 1 "%WIX%\bin\light.exe" -ext WiXNetFxExtension -ext WixUIExtension out\Product.wixobj -out out\MarkdownEditSetup.msi -sice:ICE61
-@if NOT ERRORLEVEL 1 mv out\*.msi ..\..
+@if NOT ERRORLEVEL 1 mv "out\*.msi" "..\.."
 exit /b errorlevel
