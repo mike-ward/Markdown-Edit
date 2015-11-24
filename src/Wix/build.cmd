3b589a46f6ec41a730e91b35022e450660ec5782
mike-ward <mike@mike-ward.net>
2015-11-23 19:08:25 -0500

    appveyor stuff

--- src/Wix/build.cmd
+++ src/Wix/build.cmd
@@ -1,5 +1,6 @@
 del /Q "out\*.msi"
+md out
 @"%WIX%\bin\candle.exe" Product.wxs -dSourceFiles="..\MarkdownEdit\bin\Release" -out out\
 @if NOT ERRORLEVEL 1 "%WIX%\bin\light.exe" -ext WiXNetFxExtension -ext WixUIExtension out\Product.wixobj -out out\MarkdownEditSetup.msi -sice:ICE61
-@if NOT ERRORLEVEL 1 mv "out\*.msi" "..\.."
+@if NOT ERRORLEVEL 1 mv "out\MarkdownEditSetup.msi" "..\.."
 exit /b errorlevel
