060cc52060700220708509ea11a3582c097722b8
Mike Ward <mike@mike-ward.net>
2015-11-29 13:55:13 -0500

    Update comment

--- assets/js/mathjax-scripts.js
+++ assets/js/mathjax-scripts.js
@@ -1,4 +1,4 @@
-// Avoid undefined script pop up messages by prefacing with window.
+// Avoid undefined script pop up messages by prefixing with window.
 window.document.addEventListener("previewUpdated", function () {
   if (window.MathJax) { 
     window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
