83fbda645966bbd88c5e0e2f11a886f50c627f50
mike-ward <mike@mike-ward.net>
2016-04-24 14:41:32 -0400

    cleanup

--- src/MarkdownEdit/Snippets/SnippetManager.cs
+++ src/MarkdownEdit/Snippets/SnippetManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Windows;
 using ICSharpCode.AvalonEdit.Snippets;
 using MarkdownEdit.Models;
 using MarkdownEdit.Properties;
