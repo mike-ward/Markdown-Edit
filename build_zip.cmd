2f3e245356da51b234268fd4faf03faf747dca89
mike-ward <mike@mike-ward.net>
2016-05-29 09:58:46 -0400

    1.27

--- build_zip.cmd
+++ build_zip.cmd
@@ -1,3 +1,4 @@
+if EXIST MarkdownEdit.zip del MarkdownEdit.zip
 pushd src\MarkdownEdit\bin\Release
 "%ProgramFiles(x86)%\7-Zip\7z.exe" a -x!*.vshost.* -x!*.xml -x!lib/*.*  -r ..\..\..\..\MarkdownEdit.zip *.* -mmt
 popd 
