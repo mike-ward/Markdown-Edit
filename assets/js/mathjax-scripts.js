f934bf2ca5d2ff92a2c2fcba4e029ba4b9222af7
Mike Ward <mike@mike-ward.net>
2016-06-24 09:30:10 -0400

    Undo last checkin. Big mistake.

--- assets/js/mathjax-scripts.js
+++ assets/js/mathjax-scripts.js
@@ -1,5 +1,5 @@
 // Avoid undefined script pop up messages by prefixing with window.
-window.MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+// window.MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
 window.document.addEventListener("previewUpdated", function () {
   if (window.MathJax) { 
     window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
