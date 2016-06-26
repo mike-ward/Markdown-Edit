70b9190e29c5802f36f923060f88fd3f8681e9e5
Joshua Carmody <contact@joshuacarmody.com>
2016-06-26 08:24:00 -0400

    Created unit tests for StringExtensions.SurroundWith method. (#158)

---
+++ src/UnitTests/Models/StringExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MarkdownEdit.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void SurroundWithShouldReturnQuotedStringGivenQuotes()
+        {
+            var result = "Markdown".SurroundWith("\"");
+            result.ShouldBeEquivalentTo("\"Markdown\"");
+        }
+
+        [TestMethod]
+        public void SurroundWithShouldReturnOriginalStringGivenEmptyString()
+        {
+            var result = "Markdown".SurroundWith(string.Empty);
+            result.ShouldBeEquivalentTo("Markdown");
+        }
+
+        [TestMethod]
+        public void SurroundWithShouldReturnTwoQuotesGivenQuotesAndCalledOnEmptyString()
+        {
+            var result = String.Empty.SurroundWith("\"");
+            result.ShouldBeEquivalentTo("\"\"");
+        }
+
+        [TestMethod]
+        public void SurroundWithShouldReturnTwoQuotesGivenQuotesAndCalledOnNull()
+        {
+            string originalString = null;
+            var result = originalString.SurroundWith("\"");
+            result.ShouldBeEquivalentTo("\"\"");
+        }
+
+        [TestMethod]
+        public void SurroundWithShouldReturnEmptyStringGivenNullAndCalledOnNull()
+        {
+            string originalString = null;
+            var result = originalString.SurroundWith(null);
+            result.ShouldBeEquivalentTo(string.Empty);
+        }
+
+        [TestMethod]
+        public void SurroundWithShouldReturnOriginalStringGivenNull()
+        {
+            var result = "Markdown".SurroundWith(null);
+            result.ShouldBeEquivalentTo("Markdown");
+        }
+    }
+}
