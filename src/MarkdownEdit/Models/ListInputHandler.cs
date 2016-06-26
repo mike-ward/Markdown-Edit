ceb40502d290f0857c3df3b2782ac0bbc1fd0920
mike-ward <mike@mike-ward.net>
2016-06-26 17:51:50 -0400

    refactor from command to model

--- src/MarkdownEdit/Models/ListInputHandler.cs
+++ src/MarkdownEdit/Models/ListInputHandler.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using ICSharpCode.AvalonEdit;
 using ICSharpCode.AvalonEdit.Document;
+using PM = System.Tuple<System.Text.RegularExpressions.Regex, System.Action<System.Text.RegularExpressions.Match>>;
 
 namespace MarkdownEdit.Models
 {
@@ -11,52 +13,45 @@ namespace MarkdownEdit.Models
 
     public static class ListInputHandler
     {
-        public static readonly Regex UnorderedListCheckboxPattern =
-            new Regex(@"^[ ]{0,3}[-\*\+][ ]{1,3}\[[ xX]\](?=[ ]{1,3}\S)", RegexOptions.Compiled);
-
-        public static readonly Regex UnorderedListCheckboxEndPattern = new Regex(
-            @"^[ ]{0,3}[-\*\+][ ]{1,3}\[[ xX]\]\s*", RegexOptions.Compiled);
-
-        public static readonly Regex OrderedListPattern = new Regex(@"^[ ]{0,3}(\d+)\.(?=[ ]{1,3}\S)",
-            RegexOptions.Compiled);
-
-        public static readonly Regex OrderedListEndPattern = new Regex(@"^[ ]{0,3}(\d+)\.(?=[ ]{1,3}\s*)",
-            RegexOptions.Compiled);
-
-        public static readonly Regex UnorderedListPattern = new Regex(@"^[ ]{0,3}[-\*\+](?=[ ]{1,3}\S)",
-            RegexOptions.Compiled);
-
-        public static readonly Regex UnorderedListEndPattern = new Regex(@"^[ ]{0,3}[-\*\+](?=[ ]{1,3}\s*)",
-            RegexOptions.Compiled);
-
+        public static readonly Regex UnorderedListCheckboxPattern = new Regex(@"^[ ]{0,3}[-\*\+][ ]{1,3}\[[ xX]\](?=[ ]{1,3}\S)", RegexOptions.Compiled);
+        public static readonly Regex UnorderedListCheckboxEndPattern = new Regex(@"^[ ]{0,3}[-\*\+][ ]{1,3}\[[ xX]\]\s*", RegexOptions.Compiled);
+        public static readonly Regex OrderedListPattern = new Regex(@"^[ ]{0,3}(\d+)\.(?=[ ]{1,3}\S)", RegexOptions.Compiled);
+        public static readonly Regex OrderedListEndPattern = new Regex(@"^[ ]{0,3}(\d+)\.(?=[ ]{1,3}\s*)", RegexOptions.Compiled);
+        public static readonly Regex UnorderedListPattern = new Regex(@"^[ ]{0,3}[-\*\+](?=[ ]{1,3}\S)", RegexOptions.Compiled);
+        public static readonly Regex UnorderedListEndPattern = new Regex(@"^[ ]{0,3}[-\*\+](?=[ ]{1,3}\s*)", RegexOptions.Compiled);
         public static readonly Regex BlockQuotePattern = new Regex(@"^(([ ]{0,4}>)+)[ ]{0,4}.{2}", RegexOptions.Compiled);
         public static readonly Regex BlockQuoteEndPattern = new Regex(@"^([ ]{0,4}>)+[ ]{0,4}\s*", RegexOptions.Compiled);
 
-        public static void AdjustList(TextEditor editor, bool lineCountGrowing)
+        public static void AdjustList(TextEditor editor, bool lineCountIncreased)
         {
             var document = editor.Document;
             var line = document.GetLineByOffset(editor.SelectionStart)?.PreviousLine;
             if (line == null) return;
             var text = document.GetText(line.Offset, line.Length);
 
-            Func<Regex, Action<Match>, Action> matchDo = (pattern, action) =>
+            // A poor mans pattern matcher
+
+            Action<string, IEnumerable<PM>> match = (txt, patterns) =>
             {
-                var match = pattern.Match(text);
-                return match.Success ? () => action(match) : (Action)null;
+                var firstMatch = patterns
+                    .Select(pattern => new {Match = pattern.Item1.Match(txt), Action = pattern.Item2})
+                    .FirstOrDefault(ma => ma.Match.Success);
+
+                firstMatch?.Action(firstMatch.Match);
             };
 
-            var patterns = new[]
+            document.BeginUpdate();
+
+            match(text, new[]
             {
-                matchDo(UnorderedListCheckboxPattern,
-                    m => document.Insert(editor.SelectionStart, m.Groups[0].Value.TrimStart() + " ")),
-                matchDo(UnorderedListCheckboxEndPattern, m => document.Remove(line)),
-                matchDo(UnorderedListPattern,
-                    m => document.Insert(editor.SelectionStart, m.Groups[0].Value.TrimStart() + " ")),
-                matchDo(UnorderedListEndPattern, m => document.Remove(line)),
-                matchDo(OrderedListPattern, m =>
+                new PM(UnorderedListCheckboxPattern, m => document.Insert(editor.SelectionStart, m.Groups[0].Value.TrimStart() + " ")),
+                new PM(UnorderedListCheckboxEndPattern, m => document.Remove(line)),
+                new PM(UnorderedListPattern, m => document.Insert(editor.SelectionStart, m.Groups[0].Value.TrimStart() + " ")),
+                new PM(UnorderedListEndPattern, m => document.Remove(line)),
+                new PM(OrderedListPattern, m =>
                 {
                     var number = int.Parse(m.Groups[1].Value);
-                    if (lineCountGrowing)
+                    if (lineCountIncreased)
                     {
                         number += 1;
                         document.Insert(editor.SelectionStart, number + ". ");
@@ -64,12 +59,12 @@ namespace MarkdownEdit.Models
                     }
                     RenumberOrderedList(document, line, number);
                 }),
-                matchDo(OrderedListEndPattern, m => document.Remove(line)),
-                matchDo(BlockQuotePattern, m => document.Insert(editor.SelectionStart, m.Groups[1].Value.TrimStart())),
-                matchDo(BlockQuoteEndPattern, m => document.Remove(line))
-            };
+                new PM(OrderedListEndPattern, m => document.Remove(line)),
+                new PM(BlockQuotePattern, m => document.Insert(editor.SelectionStart, m.Groups[1].Value.TrimStart())),
+                new PM(BlockQuoteEndPattern, m => document.Remove(line))
+            });
 
-            patterns.FirstOrDefault(action => action != null)?.Invoke();
+            document.EndUpdate();
         }
 
         private static void RenumberOrderedList(
