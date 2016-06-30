2e5f5e063f6b192358fcd857a8b20e30fd8705c9
mike-ward <mike@mike-ward.net>
2016-06-30 19:07:22 -0400

    update packages, add some tests

---
+++ src/UnitTests/Models/FileExtensionTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentAssertions;
+using MarkdownEdit.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class FileExtensionTests
+    {
+        [TestMethod]
+        public void MakeRelativePathShouldReturnRelativePath()
+        {
+            var path = FileExtensions.MakeRelativePath(@"file://mystuff/test.txt", @"file://log/stuff");
+            path.Should().Be(@"file://log/stuff");
+        }
+
+        [TestMethod]
+        public void MakeReleativePathShouldThrowWhenFirstArgNull()
+        {
+            Action act = () => FileExtensions.MakeRelativePath(null, @"file://somestuff");
+            act.ShouldThrow<ArgumentNullException>().Where(e => e.ParamName == "fromPath");
+        }
+
+        [TestMethod]
+        public void MakeReleativePathShouldThrowWhenSecondeArgNull()
+        {
+            Action act = () => FileExtensions.MakeRelativePath(@"file://somestuff", null);
+            act.ShouldThrow<ArgumentNullException>().Where(e => e.ParamName == "toPath");
+        }
+    }
+}
