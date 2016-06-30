c242dbdca0443760ea87782d58cacd2f6fcc2762
mike-ward <mike@mike-ward.net>
2016-06-30 10:50:49 -0400

    use 64 bit version so appveyor works

--- build_zip.cmd
+++ build_zip.cmd
@@ -3,7 +3,7 @@ echo ============================
 echo ====== Build Zip File ======
 echo ============================
 if EXIST MarkdownEdit.zip del MarkdownEdit.zip
-if NOT EXIST "%ProgramFiles(x86)%\7-Zip\7z.exe" echo 7-Zip not installed
+if NOT EXIST "%ProgramFiles%\7-Zip\7z.exe" echo 7-Zip not installed
 pushd src\MarkdownEdit\bin\Release
-"%ProgramFiles(x86)%\7-Zip\7z.exe" a -x!*.vshost.* -x!*.xml -x!lib/*.*  -r ..\..\..\..\MarkdownEdit.zip *.* -mmt
+"%ProgramFiles%\7-Zip\7z.exe" a -x!*.vshost.* -x!*.xml -x!lib/*.*  -r ..\..\..\..\MarkdownEdit.zip *.* -mmt
 popd
