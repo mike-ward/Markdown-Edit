267c27154cba453c761569f60991065b259e4583
mike-ward <mike@mike-ward.net>
2016-08-29 12:48:41 -0400

    fix crash

--- src/MarkdownEdit/MarkdownConverters/CustomMarkdownConverter.cs
+++ src/MarkdownEdit/MarkdownConverters/CustomMarkdownConverter.cs
@@ -18,7 +18,7 @@ namespace MarkdownEdit.MarkdownConverters
             var separator = converter.StartsWith("\"") ? new[] {'"'} : null;
             var split = converter.Split(separator, 2);
             var fileName = split[0];
-            var args = split[1];
+            var args = split.Length == 2 ? split[1] : "";
             var program = StartInfo(fileName, args, text != null);
 
             using (var process = Process.Start(program))
