bc6a46939eb5d153948f9f619e5f404e6f7c0128
mike-ward <mike@mike-ward.net>
2016-09-17 15:05:14 -0400

    create vm for main window

--- src/MarkdownEdit/Controls/MainWindow.vm.cs
+++ src/MarkdownEdit/Controls/MainWindow.vm.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using MarkdownEdit.Commands;
 using MarkdownEdit.MarkdownConverters;
+using MarkdownEdit.Models;
+using MarkdownEdit.Properties;
 using MarkdownEdit.Snippets;
 using MarkdownEdit.SpellCheck;
+using Version = MarkdownEdit.Models.Version;
 
 namespace MarkdownEdit.Controls
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        public const int EditorMarginMin = 4;
+        public const int EditorMarginMax = 16;
+
         private IMarkdownConverter _commonMarkConverter;
         private string _titleName = string.Empty;
         private Thickness _editorMargins;
@@ -18,6 +27,8 @@ namespace MarkdownEdit.Controls
         private bool _newVersion;
         private ISnippetManager _snippetManager;
         private ISpellCheckProvider _spellCheckProvider;
+
+        public MainWindow MainWindow { get; set; }
 
         public string TitleName
         {
@@ -74,6 +85,93 @@ namespace MarkdownEdit.Controls
             if (EqualityComparer<T>.Default.Equals(property, value)) return;
             property = value;
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void OnClosing(object sender, CancelEventArgs cancelEventArgs)
+        {
+            MainWindow.Editor.CloseHelp();
+            MainWindow.DisplaySettings.SaveIfModified();
+            cancelEventArgs.Cancel = !MainWindow.Editor.SaveIfModified();
+            if (cancelEventArgs.Cancel || App.UserSettings.YesIDonated) return;
+            var donate = new Donate { Owner = Application.Current.MainWindow };
+            donate.ShowDialog();
+        }
+
+        public void OnFirstActivation(object sender, EventArgs eventArgs)
+        {
+            MainWindow.Activated -= OnFirstActivation;
+            MainWindow.Dispatcher.InvokeAsync(async () =>
+            {
+                var updateMargins = Utility.Debounce(() =>
+                    MainWindow.Dispatcher.Invoke(() => EditorMargins = CalculateEditorMargins()), 50);
+
+                App.UserSettings.PropertyChanged += (o, args) =>
+                {
+                    if (args.PropertyName == nameof(App.UserSettings.SinglePaneMargin)) updateMargins();
+                    if (args.PropertyName == nameof(App.UserSettings.GitHubMarkdown)) MainWindow.Preview.UpdatePreview(MainWindow.Editor);
+                };
+                MainWindow.SizeChanged += (s, e) => updateMargins();
+                updateMargins();
+                LoadCommandLineOrLastFile();
+                Application.Current.Activated += OnActivated;
+                NewVersion = !await Version.IsCurrentVersion();
+            });
+        }
+
+        public void OnActivated(object sender, EventArgs args)
+        {
+            if (MainWindow.Preview.Visibility == Visibility.Visible && MainWindow.Editor.Visibility != Visibility.Visible)
+            {
+                MainWindow.SetFocus(MainWindow.Preview.Browser);
+            }
+        }
+        private void LoadCommandLineOrLastFile()
+        {
+            var fileToOpen = Environment.GetCommandLineArgs().Skip(1).FirstOrDefault()
+                ?? (App.UserSettings.EditorOpenLastFile ? Settings.Default.LastOpenFile : null);
+
+            if (string.IsNullOrWhiteSpace(fileToOpen)
+                || fileToOpen == "-n"
+                || !MainWindow.Editor.LoadFile(fileToOpen))
+            {
+                NewFileCommand.Command.Execute(null, MainWindow);
+            }
+        }
+
+        public void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            MainWindow.IsVisibleChanged -= OnIsVisibleChanged;
+            UpdateEditorPreviewVisibility(Settings.Default.EditPreviewHide);
+            MainWindow.Activate();
+        }
+
+        public void UpdateEditorPreviewVisibility(int state)
+        {
+            MainWindow.Editor.Visibility = state == 2 ? Visibility.Collapsed : Visibility.Visible;
+            MainWindow.PreviewAirspaceDecorator.Visibility = state == 1 ? Visibility.Collapsed : Visibility.Visible;
+            MainWindow.Preview.Visibility = MainWindow.PreviewAirspaceDecorator.Visibility;
+            MainWindow.UniformGrid.Columns = state == 0 ? 2 : 1;
+            MainWindow.SetFocus(state == 2 ? MainWindow.Preview.Browser as IInputElement : MainWindow.Editor.EditBox);
+            EditorMargins = CalculateEditorMargins();
+        }
+
+        private Thickness CalculateEditorMargins()
+        {
+            var singlePaneMargin = Math.Min(Math.Max(EditorMarginMin, App.UserSettings.SinglePaneMargin), EditorMarginMax);
+            var margin = MainWindow.UniformGrid.Columns == 1 ? MainWindow.ActualWidth / singlePaneMargin : 0;
+            return new Thickness(margin, 0, margin, 0);
+        }
+
+        public void EditorOnPropertyChanged(object sender, PropertyChangedEventArgs ea)
+        {
+            switch (ea.PropertyName)
+            {
+                case nameof(Editor.FileName):
+                case nameof(Editor.DisplayName):
+                case nameof(Editor.IsModified):
+                    TitleName = $"{App.Title} - {(MainWindow.Editor.IsModified ? "* " : "")}{MainWindow.Editor.DisplayName}";
+                    break;
+            }
         }
     }
 }
--- src/MarkdownEdit/Controls/MainWindow.xaml.cs
+++ src/MarkdownEdit/Controls/MainWindow.xaml.cs
@@ -1,23 +1,17 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Threading;
-using MarkdownEdit.Commands;
 using MarkdownEdit.Models;
-using MarkdownEdit.Properties;
 using MarkdownEdit.Snippets;
 using MarkdownEdit.SpellCheck;
-using Version = MarkdownEdit.Models.Version;
 
 namespace MarkdownEdit.Controls
 {
     public partial class MainWindow
     {
-        public const int EditorMarginMin = 4;
-        public const int EditorMarginMax = 16;
         private readonly MainWindowViewModel _vm;
 
         public MainWindow()
@@ -32,93 +26,21 @@ namespace MarkdownEdit.Controls
             InitializeComponent();
 
             _vm = (MainWindowViewModel)DataContext;
+            _vm.MainWindow = this;
             _vm.SpellCheckProvider = spellCheckProvider;
             _vm.SnippetManager = snippetManager;
 
-            Closing += OnClosing;
-            Activated += OnFirstActivation;
-            IsVisibleChanged += OnIsVisibleChanged;
-            Editor.PropertyChanged += EditorOnPropertyChanged;
+            Closing += _vm.OnClosing;
+            Activated += _vm.OnFirstActivation;
+            IsVisibleChanged += _vm.OnIsVisibleChanged;
+            Editor.PropertyChanged += _vm.EditorOnPropertyChanged;
             Editor.TextChanged += (s, e) => Preview.UpdatePreview((Editor)s);
             Editor.ScrollChanged += (s, e) => Preview.SetScrollOffset(s, e);
-        }
-
-        private void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
-        {
-            IsVisibleChanged -= OnIsVisibleChanged;
-            UpdateEditorPreviewVisibility(Settings.Default.EditPreviewHide);
-            Activate();
-        }
-
-        private void OnClosing(object sender, CancelEventArgs cancelEventArgs)
-        {
-            Editor.CloseHelp();
-            DisplaySettings.SaveIfModified();
-            cancelEventArgs.Cancel = !Editor.SaveIfModified();
-            if (cancelEventArgs.Cancel || App.UserSettings.YesIDonated) return;
-            var donate = new Donate { Owner = Application.Current.MainWindow };
-            donate.ShowDialog();
-        }
-
-        private void EditorOnPropertyChanged(object sender, PropertyChangedEventArgs ea)
-        {
-            switch (ea.PropertyName)
-            {
-                case nameof(Editor.FileName):
-                case nameof(Editor.DisplayName):
-                case nameof(Editor.IsModified):
-                    _vm.TitleName = $"{App.Title} - {(Editor.IsModified ? "* " : "")}{Editor.DisplayName}";
-                    break;
-            }
-        }
-
-        private void OnFirstActivation(object sender, EventArgs eventArgs)
-        {
-            Activated -= OnFirstActivation;
-            Dispatcher.InvokeAsync(async () =>
-            {
-                var updateMargins = Utility.Debounce(() =>
-                    Dispatcher.Invoke(() => _vm.EditorMargins = CalculateEditorMargins()), 50);
-
-                App.UserSettings.PropertyChanged += (o, args) =>
-                {
-                    if (args.PropertyName == nameof(App.UserSettings.SinglePaneMargin)) updateMargins();
-                    if (args.PropertyName == nameof(App.UserSettings.GitHubMarkdown)) Preview.UpdatePreview(Editor);
-                };
-                SizeChanged += (s, e) => updateMargins();
-                updateMargins();
-                LoadCommandLineOrLastFile();
-                Application.Current.Activated += OnActivated;
-                _vm.NewVersion = !await Version.IsCurrentVersion();
-            });
-        }
-
-        private void OnActivated(object sender, EventArgs args)
-        {
-            if (Preview.Visibility == Visibility.Visible && Editor.Visibility != Visibility.Visible)
-            {
-                SetFocus(Preview.Browser);
-            }
         }
 
         private void SettingsClosingFinished(object sender, RoutedEventArgs e)
         {
             DisplaySettings.SaveIfModified();
-        }
-
-        // Stuff that should probably go elsewhere
-
-        private void LoadCommandLineOrLastFile()
-        {
-            var fileToOpen = Environment.GetCommandLineArgs().Skip(1).FirstOrDefault()
-                ?? (App.UserSettings.EditorOpenLastFile ? Settings.Default.LastOpenFile : null);
-
-            if (string.IsNullOrWhiteSpace(fileToOpen)
-                || fileToOpen == "-n"
-                || !Editor.LoadFile(fileToOpen))
-            {
-                NewFileCommand.Command.Execute(null, this);
-            }
         }
 
         public void SetFocus(IInputElement control)
@@ -132,19 +54,7 @@ namespace MarkdownEdit.Controls
 
         public void UpdateEditorPreviewVisibility(int state)
         {
-            Editor.Visibility = state == 2 ? Visibility.Collapsed : Visibility.Visible;
-            PreviewAirspaceDecorator.Visibility = state == 1 ? Visibility.Collapsed : Visibility.Visible;
-            Preview.Visibility = PreviewAirspaceDecorator.Visibility;
-            UniformGrid.Columns = state == 0 ? 2 : 1;
-            SetFocus(state == 2 ? Preview.Browser as IInputElement : Editor.EditBox);
-            _vm.EditorMargins = CalculateEditorMargins();
-        }
-
-        public Thickness CalculateEditorMargins()
-        {
-            var singlePaneMargin = Math.Min(Math.Max(EditorMarginMin, App.UserSettings.SinglePaneMargin), EditorMarginMax);
-            var margin = UniformGrid.Columns == 1 ? ActualWidth / singlePaneMargin : 0;
-            return new Thickness(margin, 0, margin, 0);
+            _vm.UpdateEditorPreviewVisibility(state);
         }
 
         // Old School
