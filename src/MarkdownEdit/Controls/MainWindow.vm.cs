303a9f8240f703a0b8b070fde660a88af6bddb89
mike-ward <mike@mike-ward.net>
2016-09-17 12:54:15 -0400

    refactor to vm

---
+++ src/MarkdownEdit/Controls/MainWindow.vm.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using MarkdownEdit.MarkdownConverters;
+using MarkdownEdit.Snippets;
+using MarkdownEdit.SpellCheck;
+
+namespace MarkdownEdit.Controls
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private IMarkdownConverter _commonMarkConverter;
+        private string _titleName = string.Empty;
+        private Thickness _editorMargins;
+        private FindReplaceDialog _findReplaceDialog;
+        private IMarkdownConverter _githubMarkdownConverter;
+        private bool _newVersion;
+        private ISnippetManager _snippetManager;
+        private ISpellCheckProvider _spellCheckProvider;
+
+        public string TitleName
+        {
+            get { return _titleName; }
+            set { Set(ref _titleName, value); }
+        }
+
+        public Thickness EditorMargins
+        {
+            get { return _editorMargins; }
+            set { Set(ref _editorMargins, value); }
+        }
+
+        public IMarkdownConverter CommonMarkConverter
+        {
+            get { return _commonMarkConverter; }
+            set { Set(ref _commonMarkConverter, value); }
+        }
+
+        public IMarkdownConverter GitHubMarkdownConverter
+        {
+            get { return _githubMarkdownConverter; }
+            set { Set(ref _githubMarkdownConverter, value); }
+        }
+
+        public ISpellCheckProvider SpellCheckProvider
+        {
+            get { return _spellCheckProvider; }
+            set { Set(ref _spellCheckProvider, value); }
+        }
+
+        public FindReplaceDialog FindReplaceDialog
+        {
+            get { return _findReplaceDialog; }
+            set { Set(ref _findReplaceDialog, value); }
+        }
+
+        public ISnippetManager SnippetManager
+        {
+            get { return _snippetManager; }
+            set { Set(ref _snippetManager, value); }
+        }
+
+        public bool NewVersion
+        {
+            get { return _newVersion; }
+            set { Set(ref _newVersion, value); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(property, value)) return;
+            property = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- src/MarkdownEdit/Controls/MainWindow.xaml.cs
+++ src/MarkdownEdit/Controls/MainWindow.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Threading;
 using MarkdownEdit.Commands;
-using MarkdownEdit.MarkdownConverters;
 using MarkdownEdit.Models;
 using MarkdownEdit.Properties;
 using MarkdownEdit.Snippets;
@@ -17,10 +14,11 @@ using Version = MarkdownEdit.Models.Version;
 
 namespace MarkdownEdit.Controls
 {
-    public partial class MainWindow : INotifyPropertyChanged
+    public partial class MainWindow
     {
         public const int EditorMarginMin = 4;
         public const int EditorMarginMax = 16;
+        private readonly MainWindowViewModel _vm;
 
         public MainWindow()
         {
@@ -31,10 +29,11 @@ namespace MarkdownEdit.Controls
             ISpellCheckProvider spellCheckProvider,
             ISnippetManager snippetManager)
         {
-            SpellCheckProvider = spellCheckProvider;
-            SnippetManager = snippetManager;
-
             InitializeComponent();
+
+            _vm = (MainWindowViewModel)DataContext;
+            _vm.SpellCheckProvider = spellCheckProvider;
+            _vm.SnippetManager = snippetManager;
 
             Closing += OnClosing;
             Activated += OnFirstActivation;
@@ -43,78 +42,6 @@ namespace MarkdownEdit.Controls
             Editor.TextChanged += (s, e) => Preview.UpdatePreview((Editor)s);
             Editor.ScrollChanged += (s, e) => Preview.SetScrollOffset(s, e);
         }
-
-        // Properties
-        // Tried to put these in a view model. Fuck if I could get the DataContext
-        // to resolve when binding in dp's in the editor (see xaml)
-
-        private IMarkdownConverter _commonMarkConverter;
-        private string _titleName = string.Empty;
-        private Thickness _editorMargins;
-        private FindReplaceDialog _findReplaceDialog;
-        private IMarkdownConverter _githubMarkdownConverter;
-        private bool _newVersion;
-        private ISnippetManager _snippetManager;
-        private ISpellCheckProvider _spellCheckProvider;
-
-        public string TitleName
-        {
-            get { return _titleName; }
-            set { Set(ref _titleName, value); }
-        }
-
-        public Thickness EditorMargins
-        {
-            get { return _editorMargins; }
-            set { Set(ref _editorMargins, value); }
-        }
-
-        public IMarkdownConverter CommonMarkConverter
-        {
-            get { return _commonMarkConverter; }
-            set { Set(ref _commonMarkConverter, value); }
-        }
-
-        public IMarkdownConverter GitHubMarkdownConverter
-        {
-            get { return _githubMarkdownConverter; }
-            set { Set(ref _githubMarkdownConverter, value); }
-        }
-
-        public ISpellCheckProvider SpellCheckProvider
-        {
-            get { return _spellCheckProvider; }
-            set { Set(ref _spellCheckProvider, value); }
-        }
-
-        public FindReplaceDialog FindReplaceDialog
-        {
-            get { return _findReplaceDialog; }
-            set { Set(ref _findReplaceDialog, value); }
-        }
-
-        public ISnippetManager SnippetManager
-        {
-            get { return _snippetManager; }
-            set { Set(ref _snippetManager, value); }
-        }
-
-        public bool NewVersion
-        {
-            get { return _newVersion; }
-            set { Set(ref _newVersion, value); }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void Set<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(property, value)) return;
-            property = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        // Event Handlers
 
         private void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
         {
@@ -140,7 +67,7 @@ namespace MarkdownEdit.Controls
                 case nameof(Editor.FileName):
                 case nameof(Editor.DisplayName):
                 case nameof(Editor.IsModified):
-                    TitleName = $"{App.Title} - {(Editor.IsModified ? "* " : "")}{Editor.DisplayName}";
+                    _vm.TitleName = $"{App.Title} - {(Editor.IsModified ? "* " : "")}{Editor.DisplayName}";
                     break;
             }
         }
@@ -151,7 +78,7 @@ namespace MarkdownEdit.Controls
             Dispatcher.InvokeAsync(async () =>
             {
                 var updateMargins = Utility.Debounce(() =>
-                    Dispatcher.Invoke(() => EditorMargins = CalculateEditorMargins()), 50);
+                    Dispatcher.Invoke(() => _vm.EditorMargins = CalculateEditorMargins()), 50);
 
                 App.UserSettings.PropertyChanged += (o, args) =>
                 {
@@ -162,7 +89,7 @@ namespace MarkdownEdit.Controls
                 updateMargins();
                 LoadCommandLineOrLastFile();
                 Application.Current.Activated += OnActivated;
-                NewVersion = !await Version.IsCurrentVersion();
+                _vm.NewVersion = !await Version.IsCurrentVersion();
             });
         }
 
@@ -210,7 +137,7 @@ namespace MarkdownEdit.Controls
             Preview.Visibility = PreviewAirspaceDecorator.Visibility;
             UniformGrid.Columns = state == 0 ? 2 : 1;
             SetFocus(state == 2 ? Preview.Browser as IInputElement : Editor.EditBox);
-            EditorMargins = CalculateEditorMargins();
+            _vm.EditorMargins = CalculateEditorMargins();
         }
 
         public Thickness CalculateEditorMargins()
