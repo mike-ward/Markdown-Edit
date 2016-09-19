23ba96e60c5f87f5f8a7de8074485da0322f6497
mike-ward <mike@mike-ward.net>
2016-09-19 10:14:13 -0400

    messing with UWA

---
+++ build-uwa.bat
@@ -0,0 +1,2 @@
+call rimraf uwa
+desktopappconverter -Installer MarkdownEditSetup.msi -InstallerArguments "/q" -Destination "uwa" -PackageName "MarkdownEdit" -Publisher "CN=MikeWard.Net" -Version 1.3.2.0 -MakeAppx -Verbose -Sign
