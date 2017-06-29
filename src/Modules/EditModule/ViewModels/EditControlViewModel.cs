28785a6ea2da9057e8f92611c92e651561f6e797
mike-ward <mike@mike-ward.net>
2017-06-28 21:10:53 -0400

    add avalon edit'

---
+++ src/Modules/EditModule/ViewModels/EditControlViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Media;
+using Prism.Mvvm;
+
+namespace EditModule.ViewModels
+{
+    public class EditControlViewModel : BindableBase
+    {
+        private FontFamily _fontFamily = new FontFamily("Consolas");
+
+        public FontFamily Font
+        {
+            get => _fontFamily;
+            set => SetProperty(ref _fontFamily, value);
+        }
+
+        private double _fontSize = 16;
+
+        public double FontSize
+        {
+            get => _fontSize;
+            set => SetProperty(ref _fontSize, value);
+        }
+    }
+}
--- src/Modules/EditModule/Views/EditControl.xaml.cs
+++ src/Modules/EditModule/Views/EditControl.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace EditModule.Views
+﻿using System.Windows.Data;
+using ICSharpCode.AvalonEdit;
+
+namespace EditModule.Views
 {
     public partial class EditControl
     {
         public EditControl()
         {
             InitializeComponent();
+            var textEditor = InitializeTextEditor();
+            _border.Child = textEditor;
+            Dispatcher.InvokeAsync(() => textEditor.Focus());
+        }
+
+        private TextEditor InitializeTextEditor()
+        {
+            var textEditor = new TextEditor();
+            textEditor.SetBinding(FontFamilyProperty, new Binding("Font") {Source = DataContext, Mode = BindingMode.OneWay});
+            textEditor.SetBinding(FontSizeProperty, new Binding("FontSize") {Source = DataContext, Mode = BindingMode.OneWay});
+            return textEditor;
         }
     }
 }
