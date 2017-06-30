d539792c8bc521c68288ee8700d0bbf00216ba82
mike-ward <mike@mike-ward.net>
2017-06-30 16:18:58 -0400

    add utilty stuff, tests

---
+++ src/Infrastructure/Utility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class Utility
+    {
+        public static Func<TKey, TResult> Memoize<TKey, TResult>(this Func<TKey, TResult> func)
+        {
+            var cache = new ConcurrentDictionary<TKey, TResult>();
+            return key => cache.GetOrAdd(key, func);
+        }
+
+        public static Action Debounce(this Action func, int milliseconds = 300)
+        {
+            var action = Debounce<int>(_ => func(), milliseconds);
+            return () => action(0);
+        }
+
+        public static Action<T> Debounce<T>(this Action<T> func, int milliseconds = 300)
+        {
+            var last = long.MinValue;
+            return arg =>
+            {
+                try
+                {
+                    var current = Interlocked.Increment(ref last);
+                    Task.Delay(milliseconds).ContinueWith(task =>
+                    {
+                        // ReSharper disable once AccessToModifiedClosure
+                        if (current == last) func(arg);
+                        task.Dispose();
+                    });
+                }
+                catch (OverflowException)
+                {
+                    Interlocked.Exchange(ref last, long.MinValue);
+                }
+            };
+        }
+    }
+}
---
+++ src/UnitTests/Infrastructure/UtilityTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAssertions;
+using Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Infrastructure
+{
+    [TestClass]
+    public class UtilityTests
+    {
+        [TestMethod]
+        public void MemoizeShouldReturnFunction()
+        {
+            var mul = 2;
+            // ReSharper disable once AccessToModifiedClosure
+            Func<int, int> func = i => i * mul;
+            var result = func.Memoize();
+            result.Should().NotBeNull();
+            result(2).Should().Be(4);
+            mul = 3;
+            result(2).Should().Be(4);
+            result(3).Should().Be(9);
+        }
+
+        [TestMethod]
+        public void DebouceShouldReturnFunction()
+        {
+            Action<int> func = i => { };
+            var result = func.Debounce();
+            result.Should().NotBeNull();
+        }
+    }
+}
