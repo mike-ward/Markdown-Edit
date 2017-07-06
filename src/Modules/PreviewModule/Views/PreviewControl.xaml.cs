3a699e3e6870e1a73f5f32f59845733337f981b9
mike-ward <mike@mike-ward.net>
2017-07-06 09:45:18 -0400

    add error handling to UpdateBrowser

--- src/Modules/PreviewModule/Views/PreviewControl.xaml.cs
+++ src/Modules/PreviewModule/Views/PreviewControl.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Navigation;
 using Infrastructure;
 using mshtml;
 using PreviewModule.ViewModels;
@@ -18,17 +14,15 @@ namespace PreviewModule.Views
             InitializeComponent();
             _browser.Navigate(templateLoader.DefaultTemplate());
             ViewModel.UpdateBrowserDelegate = UpdateBrowser;
-            Loaded += OnLoaded;
-            //_browser.Navigating += BrowserOnNavigating;
+            Loaded += KillPopups;
         }
 
-        private PreviewControlViewModel ViewModel => (PreviewControlViewModel)DataContext;
+        private PreviewControlViewModel ViewModel => (PreviewControlViewModel) DataContext;
 
-        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        private void KillPopups(object sender, RoutedEventArgs routedEventArgs)
         {
             Task.Factory.StartNew(() =>
             {
-                // kill popups
                 dynamic activeX = _browser.GetType().InvokeMember("ActiveXInstance",
                     BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
                     null, _browser, new object[] { });
@@ -38,38 +32,17 @@ namespace PreviewModule.Views
 
         public void UpdateBrowser(string html)
         {
-            var div = GetContentsDiv();
+            var div = (_browser.Document as IHTMLDocument3)?.getElementById("content");
+            if (div == null)
+            {
+                _browser.NavigateToString(
+                    "<h1 style=\"margin: 40% 25%; text-align: center\">" +
+                    "Content element not found<br/>" +
+                    "Preview unavailable" +
+                    "</h1>");
+                return;
+            }
             div.innerHTML = html;
-        }
-
-        private IHTMLElement GetContentsDiv()
-        {
-            var document = _browser.Document as IHTMLDocument3;
-            var element = document?.getElementById("content");
-            return element;
-        }
-
-        private void BrowserOnNavigating(object sender, NavigatingCancelEventArgs ea)
-        {
-            ea.Cancel = true;
-            var url = ea.Uri?.ToString();
-            if (url?.StartsWith("file://", StringComparison.OrdinalIgnoreCase) == true) NavigateToElement(ea.Uri.OriginalString);
-            if (url?.StartsWith("about:", StringComparison.OrdinalIgnoreCase) == false) Process.Start(url);
-        }
-
-        private void NavigateToElement(string url)
-        {
-            var lastIndex = url.LastIndexOf("#", StringComparison.Ordinal);
-            if (lastIndex == -1) return;
-            var id = url.Substring(lastIndex + 1);
-            var document3 = _browser.Document as IHTMLDocument3;
-            if (document3 == null) return;
-            var element =
-                document3.getElementById(id) ??
-                document3.getElementsByName("a").Cast<IHTMLElement>().FirstOrDefault(e => e.getAttribute("name") == id);
-            if (element == null) return;
-            var document2 = _browser.Document as IHTMLDocument2;
-            document2?.parentWindow.scroll(0, element.offsetTop);
         }
     }
 }
