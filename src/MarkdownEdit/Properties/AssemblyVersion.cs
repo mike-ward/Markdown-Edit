ad6c1436188e9b1f9d525c39a8f3d1378a8efa30
mike-ward <mike@mike-ward.net>
2017-07-12 18:00:07 -0400

    add common version info

---
+++ src/MarkdownEdit/Properties/AssemblyVersion.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyDescription("Markdown Editor with Preview")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mike Ward")]
+[assembly: AssemblyProduct("Markdown Edit")]
+[assembly: AssemblyCopyright("Copyright ©  2017, Mike Ward")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.34.0")]
+[assembly: AssemblyFileVersion("1.34.0")]
