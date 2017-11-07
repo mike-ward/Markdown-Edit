688c7b6af4cc9040323dfce7ac95f0985208cec2
mike-ward <mike@mike-ward.net>
2017-11-06 20:24:52 -0500

    add icon to dialogs

--- src/Modules/ServicesModule/Services/Notify.cs
+++ src/Modules/ServicesModule/Services/Notify.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using Infrastructure;
+using MahApps.Metro.IconPacks;
 using MahApps.Metro.SimpleChildWindow;
 
 namespace ServicesModule.Services
@@ -11,15 +12,139 @@ namespace ServicesModule.Services
     {
         private readonly double _fontSize = 18;
         private readonly double _buttonWidth = 100;
-        private readonly double _buttonColumnWidth = 120;
-        private readonly Thickness _buttonThickness = new Thickness(0, 10, 0, 0);
+        private readonly double _buttonColumnWidth = 110;
+        private readonly Thickness _buttonMargin = new Thickness(0, 10, 0, 0);
+
+        public async Task<bool> Alert(string alert)
+        {
+            var dialog = Dialog();
+            var content = (Grid)dialog.Content;
+            var icon = GetMaterialIcon(PackIconMaterialKind.AlertCircle);
+            var message = MessageBlock(alert);
+            var buttons = ButtonBlock();
+
+            buttons.SetValue(Grid.RowProperty, 2);
+            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
+
+            var ok = new Button { Content = "OK", Width = _buttonWidth };
+            ok.SetValue(Grid.RowProperty, 2);
+            ok.SetValue(Grid.ColumnProperty, 1);
+            ok.Click += (sd, ea) => dialog.Close();
+
+            buttons.Children.Add(ok);
+
+            content.Children.Add(icon);
+            content.Children.Add(message);
+            content.Children.Add(buttons);
+
+            await ShowDialog(dialog);
+            return true;
+        }
+
+        public async Task<MessageBoxResult> ConfirmYesNo(string question)
+        {
+            var result = MessageBoxResult.No;
+            var dialog = Dialog();
+            var content = (Grid)dialog.Content;
+            var icon = GetMaterialIcon(PackIconMaterialKind.HelpCircle);
+            var message = MessageBlock(question);
+            var buttons = ButtonBlock();
+
+            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
+            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
+
+            var yes = new Button { Content = "_Yes", Width = _buttonWidth };
+            yes.SetValue(Grid.RowProperty, 2);
+            yes.SetValue(Grid.ColumnProperty, 1);
+            yes.Click += (sd, ea) =>
+            {
+                result = MessageBoxResult.Yes;
+                dialog.Close();
+            };
+            var no = new Button { Content = "_No", Width = _buttonWidth };
+            no.SetValue(Grid.RowProperty, 2);
+            no.SetValue(Grid.ColumnProperty, 2);
+            no.Click += (sd, ea) =>
+            {
+                result = MessageBoxResult.No;
+                dialog.Close();
+            };
+
+            buttons.Children.Add(yes);
+            buttons.Children.Add(no);
+
+            content.Children.Add(icon);
+            content.Children.Add(message);
+            content.Children.Add(buttons);
+
+            await ShowDialog(dialog);
+            return result;
+        }
+
+        public async Task<MessageBoxResult> ConfirmYesNoCancel(string question)
+        {
+            var result = MessageBoxResult.Cancel;
+            var dialog = Dialog();
+            var content = (Grid)dialog.Content;
+
+            var icon = GetMaterialIcon(PackIconMaterialKind.HelpCircle);
+            var message = MessageBlock(question);
+            var buttons = ButtonBlock();
+
+            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
+            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
+            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
+
+            var yes = new Button { Content = "_Yes", Width = _buttonWidth };
+            yes.SetValue(Grid.RowProperty, 2);
+            yes.SetValue(Grid.ColumnProperty, 1);
+            yes.Click += (sd, ea) =>
+            {
+                result = MessageBoxResult.Yes;
+                dialog.Close();
+            };
+            var no = new Button { Content = "_No", Width = _buttonWidth };
+            no.SetValue(Grid.RowProperty, 2);
+            no.SetValue(Grid.ColumnProperty, 2);
+            no.Click += (sd, ea) =>
+            {
+                result = MessageBoxResult.No;
+                dialog.Close();
+            };
+            var cancel = new Button { Content = "_Cancel", Width = _buttonWidth };
+            cancel.SetValue(Grid.ColumnProperty, 3);
+            cancel.Click += (sd, ea) =>
+            {
+                result = MessageBoxResult.Cancel;
+                dialog.Close();
+            };
+
+            buttons.Children.Add(yes);
+            buttons.Children.Add(no);
+            buttons.Children.Add(cancel);
+
+            content.Children.Add(icon);
+            content.Children.Add(message);
+            content.Children.Add(buttons);
+
+            await ShowDialog(dialog);
+            return result;
+        }
 
         private static ChildWindow Dialog()
         {
-            var grid = new Grid { Margin = new Thickness(10), MinHeight = 75 ,Width = 500 };
+            var grid = new Grid { Margin = new Thickness(20), MinHeight = 100, Width = 550 };
+
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(50)});
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(30)});
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
             var overlay = new SolidColorBrush
             {
@@ -33,128 +158,50 @@ namespace ServicesModule.Services
             {
                 Content = grid,
                 OverlayBrush = overlay,
-                Title = Constants.ProgramName,
-                TitleBarBackground = (Brush)Application.Current.MainWindow?.FindResource("GrayBrush2")
+                ShowTitleBar = false
+                
             };
 
             return dialog;
         }
 
-        public async Task<bool> Alert(string alert)
+        private static async Task ShowDialog(ChildWindow dialog)
         {
-            var dialog = Dialog();
-            var content = (Grid)dialog.Content;
-            var message = new TextBlock { Text = alert, FontSize = _fontSize };
-            message.SetValue(Grid.RowProperty, 0);
-
-            var buttons = new Grid { Margin = _buttonThickness};
-            buttons.SetValue(Grid.RowProperty, 2);
-            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
-
-            var ok = new Button { Content = new TextBlock { Text = "OK" }, Width = _buttonWidth };
-            ok.SetValue(Grid.RowProperty, 2);
-            ok.SetValue(Grid.ColumnProperty, 1);
-            ok.Click += (sd, ea) => dialog.Close();
-
-            buttons.Children.Add(ok);
-
-            content.Children.Add(message);
-            content.Children.Add(buttons);
-
-            await Application.Current.MainWindow.ShowChildWindowAsync(dialog);
-            return true;
+            await Application.Current.MainWindow.ShowChildWindowAsync(dialog, ChildWindowManager.OverlayFillBehavior.FullWindow);
         }
 
-        public async Task<MessageBoxResult> ConfirmYesNo(string question)
+        private static PackIconMaterial GetMaterialIcon(PackIconMaterialKind kind)
         {
-            var result = MessageBoxResult.No;
-            var dialog = Dialog();
-            var content = (Grid)dialog.Content;
-            var message = new TextBlock { Text = question, FontSize = _fontSize };
-            message.SetValue(Grid.RowProperty, 0);
-
-            var buttons = new Grid { Margin = _buttonThickness };
-            buttons.SetValue(Grid.RowProperty, 2);
-            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
-            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
-
-            var yes = new Button { Content = new Label { Content = "_Yes" }, Width = _buttonWidth };
-            yes.SetValue(Grid.RowProperty, 2);
-            yes.SetValue(Grid.ColumnProperty, 1);
-            yes.Click += (sd, ea) =>
+            var icon = new PackIconMaterial
             {
-                result = MessageBoxResult.Yes;
-                dialog.Close();
-            };
-            var no = new Button { Content = new Label { Content = "_No" }, Width = _buttonWidth };
-            no.SetValue(Grid.RowProperty, 2);
-            no.SetValue(Grid.ColumnProperty, 2);
-            no.Click += (sd, ea) =>
-            {
-                result = MessageBoxResult.No;
-                dialog.Close();
+                Kind = kind,
+                Width = 50,
+                Height = 50,
+                Foreground = new SolidColorBrush(Colors.SteelBlue),
+                VerticalAlignment = VerticalAlignment.Center
             };
 
-            buttons.Children.Add(yes);
-            buttons.Children.Add(no);
+            icon.SetValue(Grid.RowProperty, 0);
+            icon.SetValue(Grid.ColumnProperty, 0);
+            icon.SetValue(Grid.RowSpanProperty, 2);
 
-            content.Children.Add(message);
-            content.Children.Add(buttons);
-
-            await Application.Current.MainWindow.ShowChildWindowAsync(dialog);
-            return result;
+            return icon;
         }
 
-        public async Task<MessageBoxResult> ConfirmYesNoCancel(string question)
+        private TextBlock MessageBlock(string text)
         {
-            var result = MessageBoxResult.Cancel;
-            var dialog = Dialog();
-            var content = (Grid)dialog.Content;
-            var message = new TextBlock { Text = question, FontSize = _fontSize };
+            var message = new TextBlock { Text = text, FontSize = _fontSize };
             message.SetValue(Grid.RowProperty, 0);
+            message.SetValue(Grid.ColumnProperty, 2);
+            return message;
+        }
 
-            var buttons = new Grid { Margin = _buttonThickness };
+        private Grid ButtonBlock()
+        {
+            var buttons = new Grid { Margin = _buttonMargin };
             buttons.SetValue(Grid.RowProperty, 2);
-            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
-            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
-            buttons.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_buttonColumnWidth) });
-
-            var yes = new Button { Content = new Label { Content = "_Yes" }, Width = _buttonWidth };
-            yes.SetValue(Grid.RowProperty, 2);
-            yes.SetValue(Grid.ColumnProperty, 1);
-            yes.Click += (sd, ea) =>
-            {
-                result = MessageBoxResult.Yes;
-                dialog.Close();
-            };
-            var no = new Button { Content = new Label { Content = "_No" }, Width = _buttonWidth };
-            no.SetValue(Grid.RowProperty, 2);
-            no.SetValue(Grid.ColumnProperty, 2);
-            no.Click += (sd, ea) =>
-            {
-                result = MessageBoxResult.No;
-                dialog.Close();
-            };
-            var cancel = new Button { Content = new Label { Content = "_Cancel" }, Width = _buttonWidth };
-            cancel.SetValue(Grid.ColumnProperty, 3);
-            cancel.Click += (sd, ea) =>
-            {
-                result = MessageBoxResult.Cancel;
-                dialog.Close();
-            };
-
-            buttons.Children.Add(yes);
-            buttons.Children.Add(no);
-            buttons.Children.Add(cancel);
-
-            content.Children.Add(message);
-            content.Children.Add(buttons);
-
-            await Application.Current.MainWindow.ShowChildWindowAsync(dialog);
-            return result;
+            buttons.SetValue(Grid.ColumnProperty, 2);
+            return buttons;
         }
     }
 }
