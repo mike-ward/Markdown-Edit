bbc6326625321a1f0727e32e444bf1aa8b63c33d
mike-ward <mike@mike-ward.net>
2017-11-12 11:44:53 -0500

    add image drop dialog

---
+++ src/Modules/EditModule/ViewModels/ImageDropDialogViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Controls;
+using Prism.Mvvm;
+
+namespace EditModule.ViewModels
+{
+    public class ImageDropDialogViewModel : BindableBase
+    {
+        private string _documentFileName;
+        private bool _uploading;
+        private bool _useClipboardImage;
+
+        public string DocumentFileName
+        {
+            get => _documentFileName;
+            set => SetProperty(ref _documentFileName, value);
+        }
+
+        public bool Uploading
+        {
+            get => _uploading;
+            set => SetProperty(ref _uploading, value);
+        }
+
+        public bool UseClipboardImage
+        {
+            get => _useClipboardImage;
+            set => SetProperty(ref _useClipboardImage, value);
+        }
+
+        public ContextMenu CreateContextMenu()
+        {
+            return new ContextMenu();
+        }
+    }
+}
---
+++ src/Modules/EditModule/Views/ImageDropDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using EditModule.ViewModels;
+
+namespace EditModule.Views
+{
+    public partial class ImageDropDialog
+    {
+        private ImageDropDialogViewModel ViewModel => (ImageDropDialogViewModel)DataContext;
+
+        public ImageDropDialog()
+        {
+            InitializeComponent();
+            ContextMenu = ViewModel.CreateContextMenu();
+        }
+    }
+}
