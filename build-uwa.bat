b0f0d35dc81e4bcf78d839865b01c8fcdb607666
mike-ward <mike@mike-ward.net>
2019-01-20 11:48:45 -0500

    update packages, test installer

--- build-uwa.bat
+++
@@ -1,2 +0,0 @@
-call rimraf uwa
-desktopappconverter -Installer MarkdownEditSetup.msi -InstallerArguments "/q" -Destination "uwa" -PackageName "MarkdownEdit" -Publisher "CN=MikeWard.Net" -Version 1.3.2.0 -MakeAppx -Verbose -Sign
